e0760a71b51986f58bd92757e4daa41281c8f1f7
Svitlana <vyskrebentseva.svetlana@gmail.com>
2024-08-01 19:40:05 +0300

    Init Spring Boot application

---
+++ src/test/java/com/svysk/ms_delivery/MsDeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.svysk.ms_delivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsDeliveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
