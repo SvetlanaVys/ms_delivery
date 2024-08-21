d02ae2553c3875946f48c8fa57ed844a3aa66131
Svitlana <vyskrebentseva.svetlana@gmail.com>
2024-08-21 19:36:48 +0300

    Add basic logic for Delivery

--- src/main/java/com/svysk/ms_delivery/adapters/storage/ProductEntityDao.java
+++
@@ -1,7 +0,0 @@
-package com.svysk.ms_delivery.adapters.storage;
-
-import com.svysk.ms_delivery.adapters.storage.entity.ProductEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductEntityDao extends JpaRepository<ProductEntity, Long> {
-}
--- src/main/java/com/svysk/ms_delivery/adapters/storage/entity/DeliveryEntity.java
+++ src/main/java/com/svysk/ms_delivery/adapters/storage/entity/DeliveryEntity.java
@@ -22,7 +22,6 @@ import java.util.Objects;
 @Setter
 @ToString
 @RequiredArgsConstructor
-@NoArgsConstructor
 public class DeliveryEntity {
 
     @Id
